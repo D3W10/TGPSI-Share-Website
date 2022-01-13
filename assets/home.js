5a6e36b832cbef40e576b463c0682d72bdb44ad3
D3W10 <danyribeironunes15@gmail.com>
2022-01-13 17:29:51 +0000

    3.1

--- assets/home.js
+++ assets/home.js
@@ -1,5 +1,26 @@
 (async function () {
-    let randomQuery = null;
+    var panelSizes = {};
+
+    window.addEventListener("load", () => {
+        for (const panel of document.querySelectorAll("#card > *")) {
+            panelSizes[panel.id] = (panel.offsetHeight + 20) + "px";
+            if (panel.id == "homePanel") {
+                document.getElementById("homePanel").style.display = "block";
+                document.getElementById("homePanel").style.opacity = "1";
+                document.getElementById("card").style.height = panelSizes.homePanel;
+            }
+            else {
+                panel.style.display = "none";
+                panel.style.opacity = "0";
+            }
+        }
+    });
+
+    //#region UNSPLASH BACKGROUND
+
+    let randomQuery = null, unsplashAK = document.getElementsByTagName("meta")[5].content;
+    document.getElementsByTagName("meta")[5].remove();
+
     if (Math.floor(Math.random() * 2) == 0)
         randomQuery = "cliffs";
     else
@@ -8,14 +29,39 @@
     let upInfo = await fetch("https://api.unsplash.com/photos/random?query=" + randomQuery + "&orientation=landscape", {
         method: "GET",
         headers: {
-            Authorization: "Client-ID MkmaXl5HCFr1psAbRtB_LBIFirxvm2jBBd6whLChuWo"
+            Authorization: "Client-ID " + unsplashAK
         }
     });
     upInfo = await upInfo.json();
 
     document.body.style.backgroundImage = "url(" + upInfo.urls.full + ")";
-    document.getElementById("attribution").setAttribute("href", upInfo.user.links.html);
+    document.getElementById("attribution").setAttribute("href", upInfo.user.links.html + "?utm_source=TGPSI%20Share&utm_medium=referral");
     document.querySelector("#attribution > span").innerHTML = upInfo.user.name;
+
+    //#endregion UNSPLASH BACKGROUND
+
+    let urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get("dl") != null) {
+        document.getElementById("homePanel").style.opacity = "0";
+        await Sleep(400);
+        document.getElementById("homePanel").style.display = "none";
+        document.getElementById("card").style.height = panelSizes.downloadPanel;
+        document.getElementById("downloadPanel").style.display = "block";
+        await Sleep(400);
+        document.getElementById("downloadPanel").style.opacity = "1";
+        await Sleep(900);
+        
+        window.location.assign("tgpsi-share://download/" + urlParams.get("dl"));
+
+        await Sleep(2500);
+        document.getElementById("downloadPanel").style.opacity = "0";
+        await Sleep(400);
+        document.getElementById("downloadPanel").style.display = "none";
+        document.getElementById("card").style.height = panelSizes.homePanel;
+        document.getElementById("homePanel").style.display = "block";
+        await Sleep(400);
+        document.getElementById("homePanel").style.opacity = "1";
+    }
 }());
 
 document.getElementsByTagName("button")[0].addEventListener("click", async () => {
@@ -35,3 +81,7 @@ document.getElementsByTagName("button")[0].addEventListener("click", async () =>
 
     window.open(ghInfo[0].assets[0].browser_download_url, "_self");
 });
+
+function Sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
